24798be6669e78b49f7e59a9e9c0f13bb4a4d6d8
st67024DvorakAdam <st67024@upce.cz>
2025-02-10 15:00:37 +0100

    CV-01: Vytvoreni kostry projektu

---
+++ backend/src/main/java/cz/upce/fei/nnpiacv/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package cz.upce.fei.nnpiacv.controller;
+
+
+import cz.upce.fei.nnpiacv.service.GreetingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private GreetingService greetingService = new GreetingService();
+
+    @GetMapping("/")
+    public String sayGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
