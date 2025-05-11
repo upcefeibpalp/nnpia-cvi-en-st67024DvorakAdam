f0644a69931af79653d337f6365d2960df3c0c50
Adam Dvořák <125569481+st67024DvorakAdam@users.noreply.github.com>
2025-05-11 23:15:01 +0200

    CV10 - Create JwtResponse.java

---
+++ backend/src/main/java/cz/upce/fei/nnpiacv/dto/JwtResponse.java
@@ -0,0 +1,12 @@
+package cz.upce.fei.nnpiacv.dto;
+
+import lombok.Data;
+
+@Data
+public class JwtResponse {
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+}
